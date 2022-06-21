711bb708c6d24f9d77b675d2567beb6acab683a5
Firman Wahyudi <e41190305@student.polije.ac.id>
2022-06-21 23:22:11 +0700

    Update koneksi.php

--- code/public/koneksi/koneksi.php
+++ code/public/koneksi/koneksi.php
@@ -1,5 +1,5 @@
 <?php
-$konek = mysqli_connect("localhost", "root", "", "cucisepatu");
+$konek = mysqli_connect("172.23.0.2", "root", "rootpassword", "cucisepatu");
 
 if (mysqli_connect_error()) {
     echo "Koneksi database gagal : " . mysqli_connect_error();
